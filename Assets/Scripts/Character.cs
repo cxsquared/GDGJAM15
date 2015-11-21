0cc4d98a90c4622674c6dabf10e3a82fa8b28e20
Cxsquared <cxsquared@gmail.com>
2015-11-21 09:53:38 -0500

    Initial commit 7 hours left. One Unit is working with bugs but network code works.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,190 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Character : NetworkBehaviour {
+
+    public enum CharacterType { Solider, Mage, Hacker };
+
+    public float Speed = 5;
+    public bool isLeft;
+
+    public Character OwnCharacter;
+    public Character EnemyCharacter;
+
+    [SyncVar]
+    public int health = 10;
+    public bool isMoving = true;
+    private bool attackNexus = false;
+
+    public float damageTimeMin = 1f;
+    public float damageTimeMax = 3f;
+    private float damageTime = 2f;
+    public float elapsedDamageTime = 0f;
+    private bool canDamage = true;
+
+    public int damagePowerMin = 1;
+    public int damagePowerMax = 3;
+
+    public Nexus myNexus;
+
+	// Use this for initialization
+	void Start () {
+        // pick a random color
+        Color newColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+
+        // apply it on current object's material
+        //gameObject.GetComponent<MeshRenderer>().material.color = newColor;     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isServer)
+        {
+            return;
+        }
+
+        if (isMoving)
+        {
+            if (isLeft)
+            {
+                if (this.transform.position.x < 33f)
+                {
+                    this.transform.Translate(this.Speed * Time.deltaTime, 0, 0);
+                }
+                else
+                {
+                    isMoving = false;
+                    attackNexus = true;
+                }
+            }
+            else
+            {
+                if (this.transform.position.x > -33f)
+                {
+                    this.transform.Translate(-this.Speed * Time.deltaTime, 0, 0);
+                }
+                else
+                {
+                    isMoving = false;
+                    attackNexus = true;
+                }
+            }
+        }
+        else if (EnemyCharacter != null || attackNexus)
+        {
+            if (canDamage)
+            {
+                int damageGoingToGive = Random.Range(this.damagePowerMin, this.damagePowerMax);
+                if (!attackNexus)
+                {
+                    int enemyHealth = EnemyCharacter.health;
+                    EnemyCharacter.TakeDamage(damageGoingToGive);
+                    Debug.Log("Enemy health " + EnemyCharacter.health);
+                }
+                else
+                {
+                    Nexus[] nexuses = GameObject.FindObjectsOfType<Nexus>();
+                    foreach (Nexus nexus in nexuses) {
+                        if (nexus.isLeft != this.isLeft)
+                        {
+                            nexus.TakeDamage(damageGoingToGive);
+                            break;
+                        }
+                    }
+                }
+                canDamage = false;
+                damageTime = Random.Range(this.damageTimeMin, this.damageTimeMax);
+                elapsedDamageTime = 0f;
+            }
+            else
+            {
+                elapsedDamageTime += Time.deltaTime;
+                if (elapsedDamageTime > damageTime)
+                {
+                    canDamage = true;
+                }
+            }
+        }
+        else if (OwnCharacter != null)
+        {
+            if (OwnCharacter.isMoving)
+            {
+                this.isMoving = true;
+                this.OwnCharacter = null;
+            }
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+
+        if (collision.gameObject.tag == "Character")
+        {
+            Character collCharacter = collision.gameObject.GetComponent<Character>();
+            this.isMoving = false;
+            if (collCharacter.isLeft != this.isLeft)
+            {
+                if (EnemyCharacter == null)
+                {
+                    this.EnemyCharacter = collCharacter;
+                }
+            }
+            else if (OwnCharacter == null)
+            {
+                if (isLeft && collCharacter.gameObject.transform.position.x > this.gameObject.transform.position.x)
+                {
+                    this.OwnCharacter = collCharacter;
+                }
+                else if (collCharacter.gameObject.transform.position.x < this.gameObject.transform.position.x)
+                {
+                    this.OwnCharacter = collCharacter;
+                }
+
+            }
+        }
+    }
+
+
+    [ClientRpc]
+    void RpcDamage(int amount)
+    {
+        //Do visual things
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        this.health -= amount;
+
+        if (this.health <= 0)
+        {
+            this.health = 0;
+            this.EnemyCharacter.myNexus.ModifyResources(4);
+            if (myNexus.charactersSpawned <= 1 && myNexus.resources < 5)
+            {
+                myNexus.ModifyResources(5);
+            }
+            else
+            {
+                myNexus.ModifyResources(3);
+            }
+            myNexus.charactersSpawned--;
+            this.EnemyCharacter.isMoving = true;
+            this.EnemyCharacter = null;
+            NetworkServer.Destroy(this.gameObject);
+            return;
+        }
+
+        RpcDamage(amount);
+    }
+}
---
+++ Assets/Scripts/Nexus.cs
@@ -0,0 +1,186 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Nexus : NetworkBehaviour {
+
+    public GameObject CharacterPrefab;
+
+    public GameObject middleSpawn;
+    public GameObject topSpawn;
+    public GameObject bottomSpawn;
+
+    public GameObject blueNexus;
+    public GameObject greenNexus;
+
+    public int charactersSpawned = 0;
+
+    private NetworkIdentity identity;
+
+    private Text resourceText;
+    private int localResources = 15;
+
+    public bool isLeft = true;
+
+    [SyncVar]
+    public int Health = 25;
+    [SyncVar]
+    public int resources = 15;
+
+    public int MaxHealth = 100;
+
+	// Use this for initialization
+	void Start () {
+        identity = GetComponent<NetworkIdentity>();
+
+        if (transform.position.x > 0)
+        {
+            isLeft = false;
+            blueNexus.SetActive(true);
+            greenNexus.SetActive(false);
+            resourceText = GameObject.Find("RightResources").GetComponent<Text>();
+            //Debug.Log("Is left");
+        }
+        else
+        {
+            this.transform.localEulerAngles = new Vector3(0f, 180f, 0f);
+            blueNexus.SetActive(false);
+            greenNexus.SetActive(true);
+            resourceText = GameObject.Find("LeftResources").GetComponent<Text>();
+            //Debug.Log("is right");
+        }
+
+        RpcModifyResources(0);
+	}
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+        if (!identity.isLocalPlayer)
+        {
+            return;
+        }
+
+        if (this.resources >= 5)
+        {
+            if (Input.GetButtonDown("SpawnMiddle"))
+            {
+                CmdDoSpawnMiddle();
+                //Debug.Log("Fire! " + isServer);
+            }
+            else if (Input.GetButtonDown("SpawnTop"))
+            {
+                if (isLeft)
+                {
+                    CmdDoSpawnBottom();
+                }
+                else
+                {
+                    CmdDoSpawnTop();
+                }
+            }
+            else if (Input.GetButtonDown("SpawnBottom"))
+            {
+                if (isLeft)
+                {
+                    CmdDoSpawnTop();
+                }
+                else
+                {
+                    CmdDoSpawnBottom();
+                }
+            }
+        }
+	}
+
+    [Command]
+    void CmdDoSpawnMiddle()
+    {
+        GameObject character = (GameObject)Instantiate(CharacterPrefab, middleSpawn.transform.position ,Quaternion.identity);
+        Character charScript = character.GetComponent<Character>();
+        charScript.isLeft = this.isLeft;
+        charScript.myNexus = this;
+        NetworkServer.Spawn(character);
+        this.charactersSpawned++;
+        ModifyResources(-5);
+    }
+
+    [Command]
+    void CmdDoSpawnTop()
+    {
+        GameObject character = (GameObject)Instantiate(CharacterPrefab, topSpawn.transform.position, Quaternion.identity);
+        Character charScript = character.GetComponent<Character>();
+        charScript.isLeft = this.isLeft;
+        charScript.myNexus = this;
+        NetworkServer.Spawn(character);
+        this.charactersSpawned++;
+        ModifyResources(-5);
+    }
+
+    [Command]
+    void CmdDoSpawnBottom()
+    {
+        GameObject character = (GameObject)Instantiate(CharacterPrefab, bottomSpawn.transform.position, Quaternion.identity);
+        Character charScript = character.GetComponent<Character>();
+        charScript.isLeft = this.isLeft;
+        charScript.myNexus = this;
+        NetworkServer.Spawn(character);
+        this.charactersSpawned++;
+        ModifyResources(-5);
+    }
+
+    [ClientRpc]
+    void RpcTakeDamage(int amount)
+    {
+        //visual stuff
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        this.Health -= amount;
+        this.resources += 3;
+
+        if (this.Health <= 0)
+        {
+            NetworkServer.Destroy(this.gameObject);
+        }
+    }
+
+    [ClientRpc]
+    void RpcModifyResources(int amount)
+    {
+        if (identity.isLocalPlayer)
+        {
+            localResources += amount;
+            if (localResources < 0)
+            {
+                localResources = 0;
+            }
+            resourceText.text = this.localResources.ToString();
+        }
+        //Debug.Log("Client side text update");
+    }
+
+    public void ModifyResources(int amount)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        this.resources += amount;
+        if (this.resources < 0)
+        {
+            resources = 0;
+        }
+
+        RpcModifyResources(amount);
+    }
+}
